ece7f544e44a21f4517fc8773f2c787b7a341f6f
zakurila <zakurila@gmail.com>
2026-06-02 20:14:06 +0200

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,112 @@
+const projects = [
+
+   {
+    id: 8,
+    title: "Spring Awakening",
+    date: "Dienstag 07. April 2026",
+    shortText: `Rock-Musical von Duncan Sheik und Steven Sater`,
+    image: "https://www.volksoper.at/FrontendImageStore/2000/auto/prop/volksoper_wien/repertoire/2026-02-GP-Spring-MXS-8.jpg",
+    modalTitle: "Spring Awakening",
+    modalContent: `Rock-Musical von Duncan Sheik und Steven Sater<br>
+  In deutscher Sprache mit englischen Songs, mit deutschen und englischen Übertiteln<br>
+  Buch und Gesangstexte von Steven Sater<br>
+  Musik von Duncan Sheik<br>
+  Nach dem Schauspiel Frühlings Erwachen von Frank Wedekind<br>
+  Deutsch von Nina Schneider <br>
+    `
+  },
+
+  {
+    id: 7,
+    title: "Ausstellung Lakonisch, Ironisch, Düster. ...Und trotzdem Musik",
+    date: "10. April 2026",
+    shortText: "artP.kunstverein.",
+    image: "https://almarosequartet.com/img/artP.jpg",
+    modalTitle: "Ausstellung Lakonisch, Ironisch, Düster. ...Und trotzdem Musik",
+    modalContent: "Freitag, 10. April 2026 um 19:30<br>artP.kunstverein<br>Paul Katzbergerplatz 5<br>2380 Perchtoldsdorf"
+  },
+
+  {
+    id: 6,
+    title: "Konzertreihe „Echo des Unerhörten",
+    date: "27. Jänner 2026",
+    shortText: "Exilarte - Zentrum für verfolgte Musik<br>Palais Ehrbar – Kleiner Ehrbar Saal<br>Mühlgasse 30<br>1040 Wien",
+    image: "https://almarosequartet.com/img/alma02.jpg",
+    modalTitle: "Konzertreihe „Echo des Unerhörten",
+    modalContent: "In Erinnerung an das Leben und Wirken Alma Rosés (Wien 1906 / Auschwitz 1944) spielen Studierende und Dozenten des Alma Rosé Institut für Streichinstrumente, Gitarre und Harfe in der Musikpädagogik der mdw – Universität für Musik und darstellende Kunst Wien Werke von Hans Gál, Gideon Klein, Erwin Schulhoff u.a. <br> <br>Dienstag, 27. Jänner 2026 um 19:00 Uhr<br><br>Palais Ehrbar – Kleiner Ehrbar Saal<br>Mühlgasse 30<br>1040 Wien<br><br>Eintritt frei! .<br><br><b> Programm:</b><br><br>Gideon Klein (1919 – 1945)<br>Streichtrio (Theresienstadt 1944) Gideon Klein Trio Wien<br><br>Hiroyo Watanabe, Violine<br>Johanna Hechwartner, Viola<br>Matthäus Pescoller, Violoncello<br><br><b> Viktor Ullmann (1898 – 1944)<br>Streichquartett Nr.3, op. 46 (Theresienstadt 1943) Alma Rosé Quartett<br><br></b>Felix Aigner Violine<br>Pia Onuska, Violine<br>Marie Alma Mala Schmidt, Viola<br>Ana Šincek, Violoncello<br><br><b> Hans Gál (1890 – 1987)<br>aus: 5 Intermezzi für Streichquartett op.10 (1914) Alma Rosé Quartett</b><br><br>Felix Aigner Violine<br>Pia Onuska, Violine<br>Marie Alma Mala Schmidt, Viola<br>Ana Šincek, Violoncello<br><br>Erwin Schulhoff (1894 – 1942)<br>Fünf Stücke für Streichquartett WV 68 (1923) Noema Quartettv<br>Hiroyo Watanabe, Violine<br>Dominik Fischer, Violine<br>Elisabeth Mair, Viola<br>Matthäus Pescoller, Violoncello"
+  },
+
+    {
+    id: 5,
+    title: "Konzert in Erinnerung an Alma Rosé",
+    date: "3. November 2025",
+    shortText: "Zeit: 18.30 Uhr Ort: Haus der Geschichte Österreich. Anmeldung erforderlich!",
+    image: "https://eshte.neocities.org/marie/Photo_0745_fin.jpg",
+    modalTitle: "Konzert in Erinnerung an Alma Rosé",
+    modalContent: "Anmeldung erforderlich! Alma Rosé stammte aus einer bekannten österreichischen Musiker*innenfamilie und erlangte als Geigerin Bekanntheit. Ihre Karriere wurde 1938 durch den „Anschluss“ Österreichs an das nationalsozialistische deutsche Reich abrupt beendet. Als Dirigentin des Frauenorchesters im Konzentrations- und Vernichtungslager Auschwitz-Birkenau konnte sie das Leben zahlreicher Musikerinnen retten, bevor sie selbst 1944 in Auschwitz zu Tode kam.<br> <br>Anlässlich ihres Geburtstags und in Erinnerung an das Leben und Wirken Alma Rosés (Wien 1906 / Auschwitz 1944) spielen Studierende des Alma Rosé Institut für Streichinstrumente, Gitarre und Harfe in der Musikpädagogik Werke von Henriëtte Bosmans, Hans Gál, Gideon Klein, und Erwin Schulhoff.<br><br> Begrüßung <br> Monika Sommer, Direktorin, Haus der Geschichte Österreich <br> Gerda Müller, Vizerektorin, Universität für Musik und darstellende Kunst Wien <br> David Frühwirth führt durch das künstlerische Programm."
+  },
+
+   {
+    id: 4,
+    title: "Fremde Erde Festival",
+    date: "25. April 2025",
+    shortText: "Lesung mit musikalischer Begleitung",
+    image: "https://www.vivalaclassica.com/wp-content/uploads/2023/05/viva-A3-plakat-neu2-website-format-72dpi-i.png",
+    modalTitle: "Fremde Erde Festival",
+    modalContent: `
+      Werke von Maria Herz, Vítězslava Kaprálová, Charlotte Schlesinger.
+      <br>Texte von Maria Leitner, Anna Seghers.
+      <br>Lesung mit musikalischer Begleitung.
+      <br>Depot Kunst und Diskussion.
+    `
+  },
+  
+   {
+    id: 3,
+    title: "Venus im Pelz",
+    date: "7. März 2024",
+    shortText: `Theater Nestroyhof Hamakom, Dramatisierung der Novelle von Leopold Sacher-Masoch`,
+    image: "https://i.ds.at/WRE4mA/c:1200:901:fp:0.500:0.500/rs:fill:1600:0/plain/lido-images/2024/03/08/688b72d4-bbf0-464b-8d3a-823f28c0aa6a.jpeg",
+    modalTitle: "Venus im Pelz",
+    modalContent: `Eine Kooperation des diverCITYLAB mit Theater Nestroyhof Hamakom<br>Regie: Azelia Opak<br>Uraufführung: 7. März 2024 im Theater Nestroyhof Hamakom
+    <br>Weitere Spieltermine: 8.-10. März 2024
+    <br>Beginn jeweils um 20.00 Uhr
+    `
+  },
+
+  {
+    id: 2,
+    title: "HÖLLENKINDER",
+    date: "3., 6., 8. – 10. und 14. – 17. Dezember 2022",
+    shortText: `von Gabriele Kögl<br>Uraufführung, Koproduktion mit Theater Drachengasse<br>Bar&Co<br>um 20 Uhr`,
+    image: "https://www.drachengasse.at/upload/produktionen/produktion_1018_Photo1.jpg",
+    modalTitle: "HÖLLENKINDER",
+    modalContent: `<br>Regie: Azelia Opak<br>
+Dramaturgie: Andrea Zaiser<br>
+Bratsche: Marie Alma Mala Schmidt<br>
+Es spielt: Doris Weiner<br><br>
+Rechte bei Karl Mahnke Theaterverlag, Berlin<br><br>Dauer: 60 min.<br>
+    `
+  },
+
+  {
+    id: 1,
+    title: "CORIOLANUS",
+    date: "14. September 2022 - 16. Oktober 2022",
+    shortText: `William Shakespeare<br>Wortwiege <br> <a href="https://www.wortwiege.at/projekt/coriolanus/" target="_blank">Mehr erfahren</a>`,
+    image: "https://www.wortwiege.at/wp-content/uploads/Coriolanus-0015-c-Julia-Kampichler.jpg.webp",
+    modalTitle: "CORIOLANUS",
+    modalContent: `Sa 24.9. (19:30);
+Do 29.9. (19:30)*, So 2.10. (15:00);
+Sa 8.10. (19:30);
+Do 13.10. (19:30)*, So 16.10. (15:00)<br>* Einführungsgespräche finden am 14.9., 29.9. und 13.10. jeweils um 18:30 Uhr statt.<br><br>DAUER:<br>1 Stunde 40 Minuten, keine Pause<br><br>SPIELORT:<br>Kasematten Wiener Neustadt
+Bahngasse 27
+2700 Wiener Neustadt<br><br>Regie: Azelia Opak<br>Musik: Boglarka Bako, Marie Schmidt
+Musikbearbeitung: Barbara Maria Neu, Derya Satir<br>
+Bühne: Felix Huber<br>
+Kostüme: Noémi Borcsányi-Andits<br>
+Licht: Lukas Kaltenbäck<br>Regieassistenz: Derya Satir<br>
+Dramaturgie: Marie-Therese Handle-Pfeiffer<br>
+    `
+  },
+];
